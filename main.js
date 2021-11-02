1c1ae5740e9d2d69159fd5aaa48ac98b344115f8
Mahmoud Ashraf <mahmoudashrafahmed3@gmail.com>
2021-11-02 18:42:03 +0200

    substraction code

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 //get data
-function getData(){
-    
+function getData() {
+
     num1 = document.getElementById("N1").value;
     num2 = document.getElementById("N2").value;
     return [num1, num2];
 }
 //add
-function add(){
+function add() {
     data = getData();
     document.getElementById("result").innerHTML = (parseInt(data[0]) + parseInt(data[1]));
+}
+//substraction
+function add() {
+    data = getData();
+    document.getElementById("result").innerHTML = (parseInt(data[0]) - parseInt(data[1]));
 }
