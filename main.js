f2b50e294f838baea2f41ace7ec8136338a3055b
unknown <mustafahsan3@gmail.com>
2021-11-02 18:02:23 +0200

    handel addition function

---
+++ main.js
@@ -0,0 +1,12 @@
+//get data
+function getData(){
+    
+    num1 = document.getElementById("N1").value;
+    num2 = document.getElementById("N2").value;
+    return [num1, num2];
+}
+//add
+function add(){
+    data = getData();
+    document.getElementById("result").innerHTML = (parseInt(data[0]) + parseInt(data[1]));
+}
